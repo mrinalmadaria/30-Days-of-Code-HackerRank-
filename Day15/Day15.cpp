76cbba012fda4d17dab393e54eb0fbf2b1fe331d
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-02-02 20:03:11 +0530

    Day 15 update

---
+++ Day15/Day15.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+          Node *myNode = new Node(data);
+          if(head == NULL){
+              return myNode;
+          }
+          Node *tmp = head;
+          while(tmp->next != NULL){
+              tmp = tmp->next;
+          }
+          tmp->next = myNode;
+          return head;
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
